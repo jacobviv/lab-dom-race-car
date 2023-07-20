f7a8b05b315fc59dc92d903d4923d8c97a77167a
Ruben Briongos <ruben.briongos@gmail.com>
2023-07-20 18:33:19 +0200

    first iteration at game.js

--- js/game.js
+++ js/game.js
@@ -1,3 +1,45 @@
 class Game {
-    // code to be added
+    constructor() {
+        this.startScreen = document.getElementById("game-intro")
+        this.gameScreen = document.getElementById("game-screen")
+        this.gameEndScreen = document.getElementById("game-end")
+        this.player = null
+        this.height = 600
+        this.width = 500
+        this.obstacles = []
+        this.score = 0
+        this.lives = 3
+        this.gameIsOver = false
+    }
+
+    start() {
+        // Set the height and width of the game screen
+        this.gameScreen.style.height = `${this.height}px`
+        this.gameScreen.style.width = `${this.width}px`
+
+        // Hide the start screen
+        this.startScreen.style.display = "none"
+
+        // Show the game screen
+        this.gameScreen.style.display = "block"
+
+        // Start the game loop
+        this.gameLoop()
+    }
+
+    gameLoop() {
+        console.log("in the game loop")
+
+        // Interrupt the function to stop the loop if "gameIsOver" is set to "true"
+        if (this.gameIsOver) return
+
+        this.update()
+
+        // To ensure a consistent frame rate use window.requestAnimationFrame()
+        window.requestAnimationFrame(() => this.gameLoop())
+    }
+
+    update() {
+        console.log("in the update")
+    }
 }
